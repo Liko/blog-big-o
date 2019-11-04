93878d10e6c4699deacdfcabdb305f0c6786517a
Liko <li_xie1992@hotmail.com>
2019-11-04 12:20:54 +0000

    refactored, added for linear, logarithmic and quadratic

--- instagram_benchmark.rb
+++ instagram_benchmark.rb
@@ -1,9 +1,6 @@
 require 'benchmark'
 require 'pry'
 
-
-
-
 # data = (0..100_000_000)
 # target = 1
 
@@ -14,19 +11,46 @@ require 'pry'
 
 # data.bsearch {|num| num == target }
 
-def getFirstFollowerInArrayBenchmark(accounts)
+def performBenchmark(accounts, complexity)
 
-  Benchmark.bmbm do |x|
-    accounts.each_with_index do |account, i|
-      # binding.pry
-      x.report(:"#{account[0]}") {getFirstFollowerInArray(account)}
+  Benchmark.bm do |x|
+    accounts.each do |account|
+      followers = account[1]
+      x.report(:"#{account[0]}") {
+        if complexity == "constant"
+          getFirstFollowerInArray(followers)
+        elsif complexity == "linear"
+          retrieveRecentFollowerLinear(followers)
+        elsif complexity == "logarithmic"
+          retrieveRecentFollowerLogarithmic(followers)
+        elsif complexity == "quadratic"
+          introduceFollowersToEachother(followers)
+        end
+      }
     end
   end
-
 end
 
-def getFirstFollowerInArray(account)
-  account[1][0]
+def getFirstFollowerInArray(followers)
+  followers[0]
+end
+
+def retrieveRecentFollowerLinear(followers)
+  target = followers.count*0.9
+  followers.find {|follower| follower == target}
+end
+
+def retrieveRecentFollowerLogarithmic(followers)
+  target = followers.count*0.9
+  followers.bsearch {|follower| follower == target}
+end
+
+def introduceFollowersToEachother(followers)
+  followers.each do |follower_introducing|
+    followers.each do |follower_to_meet|
+      follower_introducing == follower_to_meet
+    end
+  end
 end
 
 # fish = [*1..1]
@@ -53,9 +77,35 @@ accounts = {
   :dad => dad
 }
 
+algorithms = [
+  "constant",
+  "linear",
+  "logarithmic",
+  "quadratic"
+]
 
 
-getFirstFollowerInArrayBenchmark(accounts)
+#O(1)
+puts "\n\nperforming constant benchmark \n\n"
+performBenchmark(accounts, "constant")
+
+#O(n)
+puts "\n\nperforming linear benchmark \n\n"
+performBenchmark(accounts, "linear")
+
+#O(log n)
+puts "\n\nperforming logarithmic benchmark \n\n"
+performBenchmark(accounts, "logarithmic")
+
+#O(n**2)
+puts "\n\nperforming quadratic benchmark \n\n"
+performBenchmark(accounts, "quadratic")
+
+
+
+
+
+
 
 
 
